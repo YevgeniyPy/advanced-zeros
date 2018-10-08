32a9221864e8797ebe65339cc5c5bf69a8d227b6
YevgeniyPy <43146121+YevgeniyPy@users.noreply.github.com>
2018-10-08 19:34:58 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ module.exports = function getZerosCount(number, base) {
   let prNum;
   let d; 
   let x=base;
+  let y;
   if (x<4){prNum=x;}
   if (!(x & (x-1))) {d=2}
   else { while (!(x & 1)) {x /= 2}
@@ -17,8 +18,11 @@ module.exports = function getZerosCount(number, base) {
   d=d-2;
   }
   prNum=d;
+  for (y=0;base%d==0;y++){
+    base /= d;
+  }
   for (let i = d ; number / i >= 1; i *= d){
     zerosCount += Math.floor(number / i);
   }
-  return zerosCount;
+  return Math.floor(zerosCount/y);
 }
